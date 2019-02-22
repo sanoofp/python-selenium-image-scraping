4b097f34da5a7485ff0e673ce8a8d3c380bb3e7a
sanoofp <sanoofpb24@gmail.com>
2019-02-22 13:30:57 +0530

    Some fixes for base64 image source's

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import urllib.request
 import random
 import os
 
+# path for the chromedriver.exe
 from config import local_path
 
 
@@ -15,22 +16,36 @@ url = input('Enter the url : ')
 driver = webdriver.Chrome(executable_path=local_path)
 driver.get(url)
 try:
+  # Wait for the webpage to load all images
   WebDriverWait(driver, 3).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'img')))
   print('OK')
 except TimeoutException:
   print('Timeout')
 
+# get all img tags and assign the 'src' attributes to returnImgs array
 imgs = driver.find_elements_by_tag_name('img')
-# imgs = driver.find_element_by_xpath('//img')
 returnImgs = []
 for img in imgs:
   returnImgs.append(img.get_attribute('src'))
   print(img.get_attribute('src'))
 
+# Loop through each source link
 for src in returnImgs:
   imgsrc = str(src)
-  imgExt = imgsrc[-4:]
+  imgExt = imgsrc[-4:] # Not reliable
+
+  # Fixes for base64 (Need better approach)
+  if(imgExt != '.png' or imgExt != '.jpg' or imgExt != '.svg'):
+    if(imgExt.find('.svg') > 0):
+      imgExt = ".svg"
+    elif(imgExt.find('jpeg') > 0):
+      imgExt = ".jpg"
+    else:
+      imgExt = ".jpg"
   
+  # Creating a local download link path with image extension
   download_link = os.path.dirname(os.path.realpath(__file__))+'/downloads/'+str(random.randint(1, 1000))+imgExt
+
   print("Downlodaing to :::: "+download_link)
+  # Downloading to ./downloads folder
   urllib.request.urlretrieve(imgsrc, download_link)
